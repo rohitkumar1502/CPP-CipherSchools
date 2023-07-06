f178bec71e29bb6007d434aab4d2eba82e03df10
rohitkumar1502 <krohitgupta1502@gmail.com>
2023-07-06 16:29:51 -0700

    address updated

--- Lacture11-CipherSchools/Address_of_element.cpp
+++ Lacture11-CipherSchools/Address_of_element.cpp
@@ -4,9 +4,13 @@ using namespace std;
 int main(){
     long long int a;
     int b;
+	
+
 	cout<<"The size of int is: "<<sizeof(b)<<endl;   
 
 	cout<<"The size of long long int is: "<<sizeof(a); 
+
+	cout<<"The address of a is "<<&a<<"  The address of b is "<<&b;
 	
 	return 0;
 }
