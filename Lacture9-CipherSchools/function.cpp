f554f606bfa3cf879009f88c056792c7aa9a442f
rohitkumar1502 <krohitgupta1502@gmail.com>
2023-07-06 07:22:07 -0700

    Some function Program willl be added

--- Lacture9-CipherSchools/function.cpp
+++ Lacture9-CipherSchools/function.cpp
@@ -1,10 +1,54 @@
-int compare(int a, int b){ //compare 2 integer and return the value
-     int c;
-     if(a>b){
-        c=a;
-     }
-     else{
-        c=b;
-     }
-     //Now c will store the maximum of a,b
+// int compare(int a, int b){ //compare 2 integer and return the value
+//      int c;
+//      if(a>b){
+//         c=a;
+//      }
+//      else{
+//         c=b;
+//      }
+//      //Now c will store the maximum of a,b
+// }
+
+
+// int compare(int a, int b){
+//   if(a>b){
+//    return a;
+//   }
+//   else{
+//    return b;
+//   }
+// }
+
+
+// #include<iostream>
+// using namespace std;
+
+// int compare(int a, int b){
+// 	if(a == 5){
+// 		return 1;
+// 	}
+// 	return 0;
+// }
+
+// int main(){
+// 	int a,b;
+// 	a=1,b=5;
+// 	cout<<compare(a,b);
+// 	return 0;
+// }
+
+#include<iostream>
+using namespace std;
+
+int sum(int a,int b){
+	return a+b;
+}
+
+int main(){
+	int a,b;
+	a=1,b=5;
+	int c;
+	c = sum(a,b);
+	cout<<c;
+	return 0;
 }
