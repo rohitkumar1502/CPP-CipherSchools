50acb788694903f205c22bdd60e80a87a235ddd5
rohitkumar1502 <krohitgupta1502@gmail.com>
2023-07-06 17:36:22 -0700

    Pointer

---
+++ Lacture12-CipherSchools/pointer1.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a=4;
+
+	float b;
+
+	int *p=&a;
+
+	cout<<"The address of a: "<<&a<<endl;
+
+	cout<<"The address of b: "<<&b<<endl;
+    
+	cout<<"The value of a: "<<*p;
+
+	// int n;
+
+	// float list[100],avg;
+	// avg = average(n,list);
+
+//    float list[100],avg;
+//     avg = average (n, list);
+}
+// float average (int a, float x[]){
+// 	sum = sum +x[i]
+// }
+
+
+
+// float average (int a, float *x){
+// 	sum = sum +x[i];
+// }
