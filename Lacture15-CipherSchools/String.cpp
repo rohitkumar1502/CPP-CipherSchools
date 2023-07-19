5812fa3d2004343c1daa4b1f55969a6f168ffb41
rohitkumar1502 <krohitgupta1502@gmail.com>
2023-07-19 09:42:34 -0700

    String Introduction

---
+++ Lacture15-CipherSchools/String.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    //	char r,o,h,i,t;
+    //
+    //	cin>>r>>o>>h>>i>>t;
+    /*
+       char name[1000];
+       cout<<"Enter your name: (Note: Enter # after end of your name: )";
+       int i=0; // initial value of i is 0
+       while(1){
+           cin>>name[i];
+           if(name[i] == '#'){
+               break;
+           }
+           i++;  //increasing by 1 every time whenuser inter
+       }
+
+
+       //   for(int i=0;i<6;i++){
+       //    	cin>>name[i];
+       // 	}
+       // 	cout<<"name is: ";
+       // 	for(int i=0;i<6;i++){
+       //    	cout<<name[i];
+       // 	}
+
+
+        i=0;  //again initialize to 0 to print from beginning
+        while(name[i] != '#'){
+           cout<<name[i];
+           i++;
+        }
+     */
+
+    string name; // string is a datatype
+    cin >> name;
+    cout << name;
+    return 0;
+
+    return 0;
+}
